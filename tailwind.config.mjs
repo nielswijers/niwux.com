9734f33862dc46059790412bac39f33aa749864e
Niels Wijers <nielswijers@gmail.com>
2024-10-28 22:16:03 +0100

    add nunitosans

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,9 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ["Nunito Sans Variable", "sans-serif"],
+      },
       container: {
         center: true,
         padding: "1rem",
