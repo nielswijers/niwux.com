2a72b55bb76bc7d44e3ef87d5b45a1f79ec7aa07
Niels Wijers <nielswijers@gmail.com>
2024-11-05 18:29:14 +0100

    add shadcn

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,18 +4,17 @@ import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import path from "path";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    icon({
-      include: {
-        mdi: ["*"], // Include all MDI icons
-      },
-    }),
-  ],
+  integrations: [tailwind({
+    applyBaseStyles: false,
+  }), icon({
+    include: {
+      mdi: ["*"], // Include all MDI icons
+    },
+  }), react()],
   vite: {
     resolve: {
       alias: {
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,43 +1,77 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+    darkMode: ["class"],
+    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {
-      fontFamily: {
-        sans: ["Nunito Sans Variable", "sans-serif"],
-        handwriting: ["Sue Ellen Francisco", "cursive"],
-      },
-      container: {
-        center: true,
-        padding: "1.5rem",
-      },
-      colors: {
-        background: "hsl(var(--background))",
-        foreground: "hsl(var(--foreground))",
-        card: "hsl(var(--card))",
-        "card-foreground": "hsl(var(--card-foreground))",
-        popover: "hsl(var(--popover))",
-        "popover-foreground": "hsl(var(--popover-foreground))",
-        primary: "hsl(var(--primary))",
-        "primary-foreground": "hsl(var(--primary-foreground))",
-        secondary: "hsl(var(--secondary))",
-        "secondary-foreground": "hsl(var(--secondary-foreground))",
-        muted: "hsl(var(--muted))",
-        "muted-foreground": "hsl(var(--muted-foreground))",
-        accent: "hsl(var(--accent))",
-        "accent-foreground": "hsl(var(--accent-foreground))",
-        destructive: "hsl(var(--destructive))",
-        "destructive-foreground": "hsl(var(--destructive-foreground))",
-        border: "hsl(var(--border))",
-        input: "hsl(var(--input))",
-        ring: "hsl(var(--ring))",
-        "chart-1": "hsl(var(--chart-1))",
-        "chart-2": "hsl(var(--chart-2))",
-        "chart-3": "hsl(var(--chart-3))",
-        "chart-4": "hsl(var(--chart-4))",
-        "chart-5": "hsl(var(--chart-5))",
-      },
-    },
+  	extend: {
+  		fontFamily: {
+  			sans: ["Nunito Sans Variable", "sans-serif"],
+  			handwriting: ["Sue Ellen Francisco", "cursive"]
+  		},
+  		container: {
+  			center: 'true',
+  			padding: '1.5rem'
+  		},
+  		colors: {
+  			background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			'card-foreground': 'hsl(var(--card-foreground))',
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			'popover-foreground': 'hsl(var(--popover-foreground))',
+  			primary: {
+  				DEFAULT: 'hsl(var(--primary))',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			'primary-foreground': 'hsl(var(--primary-foreground))',
+  			secondary: {
+  				DEFAULT: 'hsl(var(--secondary))',
+  				foreground: 'hsl(var(--secondary-foreground))'
+  			},
+  			'secondary-foreground': 'hsl(var(--secondary-foreground))',
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			'muted-foreground': 'hsl(var(--muted-foreground))',
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			'accent-foreground': 'hsl(var(--accent-foreground))',
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			'destructive-foreground': 'hsl(var(--destructive-foreground))',
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			'chart-1': 'hsl(var(--chart-1))',
+  			'chart-2': 'hsl(var(--chart-2))',
+  			'chart-3': 'hsl(var(--chart-3))',
+  			'chart-4': 'hsl(var(--chart-4))',
+  			'chart-5': 'hsl(var(--chart-5))',
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+  			}
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [require("@tailwindcss/typography"), require("tailwindcss-animate")],
 };
